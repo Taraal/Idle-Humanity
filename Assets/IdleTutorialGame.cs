601a90ca3e690e689c3ba9c9f0e25eef890a6135
Sylouan <T4raal@gmail.com>
2020-11-25 20:13:45 +0100

    Initialized project and git LFS

---
+++ Assets/IdleTutorialGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IdleTutorialGame : MonoBehaviour
+{
+    public Text coinsText;
+    public double coins;
+
+    public void Start(){
+        coins = 123;
+    }
+
+    public void Update(){
+        coinsText.text = "Coins : " + coins;
+    }
+
+    public void Click(){
+        coins += 1;
+    }
+}
